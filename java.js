18d84cd63c2a5afb37ee342eeeae69fd8b11f8b9
Aaron <aaronbaut95@gmail.com>
2022-07-18 09:49:45 -0700

    Add operator function
    Takes two numbers and an operator and runs function on the numbers

--- java.js
+++ java.js
@@ -10,3 +10,15 @@ const multiply = function(x,y) {
 const divide = function(x,y) {
     return x/y;
 }
+
+let operator = function(x,y,op) {
+    if(op==`+`) {
+        return add(x,y)
+    } else if(op==`-`) {
+        return subtract(x,y)
+    } else if (op==`x`){
+        return multiply(x,y)
+    } else if (op==`/`) {
+        return divide(x,y)
+    }
+};
